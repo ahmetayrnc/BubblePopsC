a9f284ca79548b341485723b8534bff1cf3719b6
Ahmet Ayrancioglu <ahmetayrnc98@gmail.com>
2019-11-23 13:06:30 +0300

    Generated Entitas

---
+++ Assets/BubblePopsC/Scripts/Mono/GameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace BubblePopsC.Scripts.Mono
+{
+    public class GameController : MonoBehaviour
+    {
+        private Entitas.Systems _systems;
+        private Contexts _contexts;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _contexts = Contexts.sharedInstance;
+            _systems = CreateSystems(_contexts);
+            InitWorld();
+        }
+
+        private void Update()
+        {
+            _systems.Execute();
+            _systems.Cleanup();
+        }
+
+        private static Entitas.Systems CreateSystems(Contexts contexts)
+        {
+            return new Feature("Systems")
+                    //initialize
+//                    .Add(new InitializeBoardSystem(contexts))
+                ;
+        }
+
+        private void InitWorld()
+        {
+            _systems.ActivateReactiveSystems();
+            _systems.Initialize();
+        }
+    }
+}
